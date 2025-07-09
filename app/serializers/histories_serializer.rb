1af9cbed26eb280dda7930799d2684383e3fdff9
Kapil Goyal <kapilgoyal6@gmail.com>
2025-07-09 11:46:06 +0530

    Added made by in to unit histroy

--- app/serializers/histories_serializer.rb
+++ app/serializers/histories_serializer.rb
@@ -1,5 +1,5 @@
 class HistoriesSerializer < BaseSerializer
-  attributes :id, :date, :event_type
+  attributes :id, :date, :event_type, :made_by
 
   attribute :date do |object|
     object.created_at
@@ -7,5 +7,14 @@ class HistoriesSerializer < BaseSerializer
 
   attribute :event_type do |object|
     object.object_changes
+  end
+
+  attribute :made_by do |object|
+    user = User.find_by(id: object.whodunnit)
+    if user.present?
+      {id: user.id, full_name: "#{user.first_name} #{user.last_name}".strip, profile_pic: user.profile_pic_url} rescue nil
+    else
+      nil
+    end
   end
 end
