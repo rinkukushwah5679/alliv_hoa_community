c7062dc1fafd75413f222f2728f601f9e7330461
Kapil Goyal <kapilgoyal6@gmail.com>
2025-07-14 16:40:21 +0530

    Calculate allocation for unit

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -11,7 +11,15 @@ class Unit < ApplicationRecord
 	has_one_attached :notice_document
 	# before_create :user_unit_limit_not_exceeded
 	before_create :set_unit_number
+  after_save :check_surface_area_change
+  # before_commit :update_allocation
+  after_destroy :recalculate_allocation_after_destroy
  
+  # def update_allocation
+  #   if destroyed?
+  #     # debugger
+  #   end
+  # end
   def set_unit_number
     last_unit_number = Unit.unscoped.maximum(:unit_number) || 001
     self.unit_number = last_unit_number + 1
@@ -35,5 +43,50 @@ class Unit < ApplicationRecord
 
   def full_address
     "#{street}, #{city} #{zip_code}".strip
+  end
+
+  private
+
+  def check_surface_area_change
+    # if saved_change_to_surface_area? && custom_association.units.present?
+    #   units = custom_association.units
+    #   total = units.sum(:surface_area).to_f
+    #   units.each do |unit|
+    #     percentag = (unit.surface_area.to_f / total * 100).round(2)
+    #     unit.update_column(:allocation, percentag)
+    #   end
+    #   puts "Surface area changed from #{surface_area_before_last_save} to #{surface_area}"
+    # end
+
+    return unless saved_change_to_surface_area?
+    return unless custom_association.present?
+
+    units = custom_association.units
+    total = units.sum(:surface_area).to_f
+
+    return if total.zero?
+
+    units.find_each do |unit|
+      percentage = (unit.surface_area.to_f / total * 100)
+      unit.update_column(:allocation, percentage)
+    end
+
+    Rails.logger.info "Surface area changed from #{surface_area_before_last_save} to #{surface_area}"
+  end
+
+  def recalculate_allocation_after_destroy
+    return unless custom_association.present?
+
+    units = custom_association.units
+    total = units.sum(:surface_area).to_f
+
+    return if total.zero?
+
+    units.find_each do |unit|
+      percentage = (unit.surface_area.to_f / total * 100)
+      unit.update_column(:allocation, percentage)
+    end
+
+    Rails.logger.info "Unit deleted. Allocation re-calculated for remaining units."
   end
 end
