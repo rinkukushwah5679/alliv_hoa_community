199a02b6d5089fcc259ecd46d184306a3705587c
Kapil Goyal <kapilgoyal6@gmail.com>
2025-07-29 17:15:27 +0530

    Added validation for date_of_purchase, of unit owner

--- app/models/ownership_account.rb
+++ app/models/ownership_account.rb
@@ -2,6 +2,7 @@ class OwnershipAccount < ApplicationRecord
 	has_paper_trail :on => [:update]
 	default_scope { order(created_at: :asc) }
 	validates :first_name, :last_name, presence: true
+	validates :date_of_purchase, presence: true
 	belongs_to :unit#, optional: true
 	belongs_to :user, class_name: "User", foreign_key: :unit_owner_id#, optional: true
 	before_create :set_association
