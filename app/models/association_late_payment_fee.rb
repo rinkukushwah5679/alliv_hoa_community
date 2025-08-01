ea7981aaccea9380aa1f8903c94e79890b7cf0ae
Kapil Goyal <kapilgoyal6@gmail.com>
2025-08-01 10:44:21 +0530

    Set frequency into late payment free if that is blank

--- app/models/association_late_payment_fee.rb
+++ app/models/association_late_payment_fee.rb
@@ -2,4 +2,11 @@ class AssociationLatePaymentFee < ApplicationRecord
 	# enum frequency: %w(3 7 15) #Frequency: after 3 days, after 7 days, after 15 days (checkboxes)
 	# enum :frequency, %w(Monthly OneTime)
 	enum :frequency, {"Next days" => 1, "After 3 days" => 3, "After 7 days" => 7, "After 15 days" => 15}
+	before_save :set_frequency_if_blank
+
+	def set_frequency_if_blank
+		unless frequency
+			self.frequency = 1
+		end
+	end
 end
