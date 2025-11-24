9570a244aeff64d26c1789635866b981f999509f
Kapil Goyal <kapilgoyal6@gmail.com>
2025-11-24 17:44:05 +0530

    Added flag on bank account is is_primary

--- app/serializers/bank_account_serializer.rb
+++ app/serializers/bank_account_serializer.rb
@@ -1,3 +1,3 @@
 class BankAccountSerializer < BaseSerializer
-  attributes :id, :funding_account_id, :is_epay, :stripe_bank_account_id, :available_balance, :mask, :account_purpose, :name, :description, :bank_account_type, :account_number, :routing_number, :is_active, :is_verified, :created_at, :updated_at
+  attributes :id, :is_primary, :funding_account_id, :is_epay, :stripe_bank_account_id, :available_balance, :mask, :account_purpose, :name, :description, :bank_account_type, :account_number, :routing_number, :is_active, :is_verified, :created_at, :updated_at
 end
