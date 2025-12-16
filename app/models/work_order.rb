0dde4a485d31c35877d48aaef6b0676f3154130c
Kapil Goyal <kapilgoyal6@gmail.com>
2025-12-16 16:34:24 +0530

    Added requestor_type for vendor

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -7,7 +7,7 @@ class WorkOrder < ApplicationRecord
 	enum :priority, ["Low", "Normal", "High"]
 	# enum :work_order_object, ["Unit", "Corridor", "Elevator", "Parking"] #etc
 	enum :status, ["Submitted", "Reviewed", "In Progress", "Completed", "Deferred", "Closed"]
-  enum :requestor_type, { "Resident" => "unit_owner", "Association" => "association", "Other" => "other"}
+  enum :requestor_type, { "Resident" => "unit_owner", "Board Member" => "board_member", "Association" => "association", "Other" => "other"}
 	belongs_to :user, class_name: 'User', foreign_key: :user_id
   belongs_to :vendor, class_name: 'User', foreign_key: :vendor_id, optional: true
   belongs_to :category, optional: true
@@ -21,8 +21,9 @@ class WorkOrder < ApplicationRecord
   def self.data_requestor_type(user_id)
     user = User.find_by(id: user_id)
     return ["Resident"] if user.present? && user.current_role == "Resident"
-    return ["BoardMember"] if user.present? && user.current_role == "BoardMember"
-    return ["Resident", "Association", "Other"]
+    return ["Board Member"] if user.present? && user.current_role == "BoardMember"
+    return ["Resident", "Board Member", "Association", "Other"] if user.present? && user.current_role == "Vendor"
+    return ["Resident", "Association", "Other"] #For System Admin
   end
 
   # def set_unit_owner_id
