b3c8a0311242ca9ff616b7e99196dacfe90ebef1
Kapil Goyal <kapilgoyal6@gmail.com>
2025-12-18 17:10:44 +0530

    Added attribute in to amenities_serializer

--- app/serializers/amenities_serializer.rb
+++ app/serializers/amenities_serializer.rb
@@ -1,5 +1,5 @@
 class AmenitiesSerializer < BaseSerializer
-  attributes :id, :auto_generate_id, :created_date, :association_name, :amenity_name, :serial_number_sku, :quantity
+  attributes :id, :auto_generate_id, :created_date, :association_name, :amenity_name, :description, :serial_number_sku, :location, :serial_number_sku, :quantity
 
   attribute :created_date do |object|
     object.created_at.strftime("%m/%d/%Y")
