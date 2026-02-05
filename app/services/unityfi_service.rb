34e0fbff0f6c421d71a5d2f395af6231021ded5d
Rinku Kushwah <rinku@bitterntec.com>
2026-02-05 14:10:37 +0530

    Fixed unityfi token error

--- app/services/unityfi_service.rb
+++ app/services/unityfi_service.rb
@@ -5,47 +5,56 @@ require "net/http"
 class UnityfiService
 
 	def generate_token
+		begin
+			unityfi = UnityfiAuthenticate.last
+			return unityfi if unityfi.present? && unityfi&.token.present? && Time.now < unityfi.token_expires_at
 
-		unityfi = UnityfiAuthenticate.last
-		return unityfi if unityfi.present? && unityfi&.token.present? && Time.now < unityfi.token_expires_at
+			url = URI("#{ENV['UNITYFI_BASE_URL']}/authentication")
 
-		url = URI("#{ENV['UNITYFI_BASE_URL']}/authentication")
+			https = Net::HTTP.new(url.host, url.port)
+			https.use_ssl = true
 
-		https = Net::HTTP.new(url.host, url.port)
-		https.use_ssl = true
+			request = Net::HTTP::Post.new(url)
+			request["Content-Type"] = "application/json"
+			request.body = JSON.dump({
+			  "APIKey": ENV['UNITYFI_API_KEY'],
+			  "Password": ENV['UNITYFI_PASSWORD']
+			})
 
-		request = Net::HTTP::Post.new(url)
-		request["Content-Type"] = "application/json"
-		request.body = JSON.dump({
-		  "APIKey": ENV['UNITYFI_API_KEY'],
-		  "Password": ENV['UNITYFI_PASSWORD']
-		})
-
-		response = https.request(request)
-		if response.code == "200"
-			data = JSON.parse(response.body)
-			if unityfi.present?
-        # update existing record
-        unityfi.update(
-          token: data["AuthenticationToken"],
-          token_expires_at: Time.now + 1.hour
-        )
-      else
-        # create new record
-        unityfi = UnityfiAuthenticate.create(
-          token: data["AuthenticationToken"],
-          company_id: data["CompanyId"],
-          token_expires_at: Time.now + 1.hour
-        )
-      end
+			response = https.request(request)
+			if response.code == "200"
+				data = JSON.parse(response.body)
+				if unityfi.present?
+	        # update existing record
+	        unityfi.update(
+	          token: data["AuthenticationToken"],
+	          token_expires_at: Time.now + 30.minutes
+	        )
+	      else
+	        # create new record
+	        unityfi = UnityfiAuthenticate.create(
+	          token: data["AuthenticationToken"],
+	          company_id: data["CompanyId"],
+	          token_expires_at: Time.now + 30.minutes
+	        )
+	      end
+			end
+			unityfi.reload
+		rescue JSON::ParserError => e
+			return {error: e.message}
+		rescue StandardError => e
+			return {error: e.message}
 		end
-		unityfi
 	end
 
 	# Create Location User (User Or Association)
 	def create_location_user(assocation)
 		user = assocation.user
 		unityfi = generate_token
+		if unityfi[:error].present?
+			Rails.logger.info "\e[31m=============Error from Unityfi when generate token for create location: #{unityfi[:error]}==============\e[0m"
+			return
+		end
 		url = URI("#{ENV['UNITYFI_BASE_URL']}/locationuser")
 
 		https = Net::HTTP.new(url.host, url.port)
@@ -83,6 +92,10 @@ class UnityfiService
 	# Create Funding Account, Bank account
 	def create_funding_account(location_user, bank)
 		unityfi = generate_token
+		if unityfi[:error].present?
+			Rails.logger.info "\e[31m=============Error from Unityfi when generate token for create funding account like bank: #{unityfi[:error]}==============\e[0m"
+			return
+		end
 		url = URI("#{ENV['UNITYFI_BASE_URL']}/locationuser/#{location_user.location_user_id}/fundingaccount")
 
 		https = Net::HTTP.new(url.host, url.port)
